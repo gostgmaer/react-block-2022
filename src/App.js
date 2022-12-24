04c06aba84a5256aa5f7b98d254cd8bd0ebdf4f8
Kishor Sarkar <kishor.sarkar@inadev.com>
2022-12-24 22:08:36 +0530

    Router and package has been installed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Fragment, React } from "react";
+import { ToastContainer } from "react-toastify";
+import Header from "./Components/Header/Header";
+import Navigation from "./Utils/Routing/Navigation";
+import { Footer } from "./Components/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div className="App">
+        <Header></Header>
+        <Navigation></Navigation>
+        <Footer></Footer>
+      </div>
+      <ToastContainer></ToastContainer>
+    </Fragment>
   );
 }
 
---
+++ src/Utils/Routing/Navigation.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { toast } from 'react-toastify';
+import Home from '../../Pages/Home/Home';
+import Error from '../../Pages/Notfound/Error';
+import About from '../../Pages/Others/About';
+import Contactus from '../../Pages/Others/Contactus';
+import Disclaimer from '../../Pages/Others/Disclaimer';
+import Privacy from '../../Pages/Others/Privacy';
+import Terms from '../../Pages/Others/terms';
+import Post from '../../Pages/Post/Post';
+import Posts from '../../Pages/Posts/Posts';
+import Search from '../../Pages/Search/Search';
+
+const Navigation = () => {
+  return (
+    <React.Fragment>
+        <Routes>
+            <Route path='/' element={<Home></Home>} ></Route>
+            <Route path='/q' element={<Search></Search>} ></Route>
+            <Route path='/disclaimer' element={<Disclaimer></Disclaimer>} ></Route>
+            <Route path='/privacy-policy' element={<Privacy></Privacy>} ></Route>
+            <Route path='/terms-acontions' element={<Terms></Terms>} ></Route>
+            <Route path='/contact' element={<Contactus/>} ></Route>
+            <Route path='/about' element={<About></About>} ></Route>
+            <Route path='/posts/:category/:name' element={<Posts></Posts>} ></Route>
+            <Route path='/posts/:tags/:name' element={<Posts></Posts>} ></Route>
+            <Route path='/posts' element={<Posts></Posts>} ></Route>
+            <Route path='/post/:title' element={<Post></Post>} ></Route>
+            <Route path='*' element={<Error></Error>} ></Route>
+        </Routes>
+    </React.Fragment>
+  )
+}
+
+export default Navigation
